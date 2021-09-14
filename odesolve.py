02c5f62fe75f753f3329556a374f23286fad0e4d
shreyaspandit <88088407+shreyaspandit@users.noreply.github.com>
2021-09-14 21:53:32 +0100

    Update odesolve.py

--- odesolve.py
+++ odesolve.py
@@ -1,21 +1,21 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
-
 class ODE(object):
     def __init__(self, initial_conditions, times, functions):
-        self.states = initial_conditions
-        self.initial_conditions = list(initial_conditions.values())
-        self.times = times
-        self.functions = functions
-        self.number_of_equations = len(self.functions)
+        self.states = initial_conditions # initial_conditions is a dictionary eg. {"t": 0, "y_1": 1, "y_2": 2...}
+        self.initial_conditions = list(initial_conditions.values()) # from above [0, 1, 2, ...]
+        self.times = times # array with two elements: start time and end time
+        self.functions = functions # name of function
+        self.number_of_equations = len(self.functions) # the number of DEs to solve
     
     def solve(self, step_size):
-        """Initialize matrix with all zeros. The rows are for the timestamps and the columns of the value
-        of each Y_k at that time."""
-        values = np.zeros((int((self.times[1]-self.times[0])/step_size) + 1, self.number_of_equations + 1))
-        values[0] = self.initial_conditions
+        
+        """
+        
+        Initialize matrix with all zeros. Each row is for a timestamp, and each column is for the value of Y_k at
+        that timestamp. Ie. row i gives the values for each of t, Y_1, ... Y_n at time i.
+        """
+        
+        values = np.zeros(( int((self.times[1]-self.times[0])/step_size) + 1, self.number_of_equations + 1 ))
+        values[0] = self.initial_conditions # set first row as initial conditions
         
         # Set timestamps
         for i in range(1, values.shape[0]):
@@ -39,11 +39,12 @@ class ODE(object):
             for i in range(1, len(self.functions)+1):
                 axs[i-1].scatter(output[:, 0], output[:, i], s = 1)
                 axs[i-1].set_xlabel(list(self.states.keys())[0])
-                axs[i-1].set_ylabel(list(self.states.keys())[i])
+                axs[i-1].set_ylabel(list(self.states.keys())[i])         
         elif multiple == False and phase == False:
             for i in range(1, len(self.functions)+1):
                 #plt.scatter(output[:, 0], output[:, i], s = 1, label = list(self.states.keys())[i])
                 plt.plot(output[:, 0], output[:, i], label=list(self.states.keys())[i], linewidth=1)
+            plt.legend(loc=2) 
         else:
             if self.number_of_equations > 2:
                 return "Error"
@@ -58,17 +59,5 @@ class ODE(object):
                 plt.plot(dat[:,0], dat[:,1])
             
         
-        plt.legend(loc=2)    
+           
         plt.show()
-        
-def dxdt(t, x):
-    return x
-
-functions = [dxdt]
-
-initial_conditions = {"t": 0.00, "x": 1.00}
-times = [-5.00, 10.00]
-
-equations = ODE(initial_conditions, times, functions)
-output = equations.solve(0.001)
-equations.plot(output)
